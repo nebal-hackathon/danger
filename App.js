ad0f43eb0635a4213f0c6d6b0f17eb36013f0da5
Yeonjun Lim <yjroot@gmail.com>
2018-03-31 03:29:06 +0900

    Add router and pages

--- App.js
+++ App.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import SplashPage from './pages/Splash'
+import MainPage from './pages/Main'
+import MapPage from './pages/Map'
+
 export default class App extends React.Component {
-  state = {
-    latitude: null,
-    longitude: null,
+  constructor(props) {
+    super(props)
+    this.state = { page: 'Splash' }
   }
-
-  componentWillMount() {
-    setInterval(() => {
-      const pos = navigator.geolocation.getCurrentPosition(pos => {
-        this.setState({
-          latitude: pos.coords.latitude,
-          longitude: pos.coords.longitude,
-        })
-      })
-    }, 1000)
+  go(page) {
+    this.setState({ page })
   }
-
   render() {
+    if (this.state.page === 'Splash') {
+      return (<SplashPage go={ (page) => this.go(page) }/>)
+    } else if (this.state.page == 'Main') {
+      return (<MainPage />)
+    } else if (this.state.page == 'Map') {
+      return (<MapPage />)
+    }
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Text>{this.state.latitude}</Text>
-        <Text>{this.state.longitude}</Text>
       </View>
     );
   }
