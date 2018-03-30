441a28a4529eeb5b6480840467a4d2735972dbbe
wshid <kso4013@gmail.com>
2018-03-31 08:14:35 +0900

    rename function

--- utils/getAirQuality.js
+++ utils/getAirQuality.js
@@ -1,6 +1,6 @@
 import { query } from 'BigQuery'
 
-export const getCrime = async ({latitude, longitude }) => {
+export const getAirQuality = async ({latitude, longitude }) => {
 	return (await query(`SELECT *, POW(ABS(${latitude} - latitude),2) + POW((${longitude} - longitude),2) AS distance
 				FROM [bigquery-public-data:openaq.global_air_quality]
 				WHERE pollutant='pm25'
