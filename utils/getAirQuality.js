060a095ed103b7af4633a796c0bb7213ca0d3c6a
wshid <kso4013@gmail.com>
2018-03-31 08:12:09 +0900

    add condition pm25

--- utils/getAirQuality.js
+++ utils/getAirQuality.js
@@ -3,6 +3,7 @@ import { query } from 'BigQuery'
 export const getCrime = async ({latitude, longitude }) => {
 	return (await query(`SELECT *, POW(ABS(${latitude} - latitude),2) + POW((${longitude} - longitude),2) AS distance
 				FROM [bigquery-public-data:openaq.global_air_quality]
+				WHERE pollutant='pm25'
 				ORDER BY distance desc
 				LIMIT 1;
 				`))
